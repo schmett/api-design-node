781eb325e1f487a7d91626b66e7a5386ce5aff92
Sylvia Yanakieva <sylvia.yanakieva@gmail.com>
2016-11-24 14:25:50 -0800

    Implement refresher 1

--- server.js
+++ server.js
@@ -2,4 +2,27 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
+// module fs - allows you to read files
+// it has a method on it - read - which allows you to read files
+
 var jsonData = {count: 12, message: 'hey'};
+
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html', function(err) {
+		if (err) {
+			res.status(500).end();
+		}
+	})
+});
+
+app.get('/data', function(req, res) {
+  res.json(jsonData);
+});
+
+app.listen(3000, function() {
+	console.log('Listening on port 3000');
+});
