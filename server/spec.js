d3759eb9ff0fd2e1b2ff6d8a3eb25d78e422d11c
Sylvia Yanakieva <sylvia.yanakieva@gmail.com>
2016-11-29 14:52:23 -0800

    Implement refresher 5

--- server/spec.js
+++ server/spec.js
@@ -8,17 +8,84 @@ var expect = require('chai').expect;
 
 describe('[LIONS]', function(){
 
-  it('should get all lions', function(done) {
+  it('should GET all lions', function(done) {
     request(app)
       .get('/lions')
       .set('Accept', 'application/json')
+      // expect below belongs to supertest
       .expect('Content-Type', /json/)
       .expect(200)
       .end(function(err, resp) {
+        // this comes from chai
         expect(resp.body).to.be.an('array');
         done();
       })
   });
+
+  it('should POST a lion', function(done) {
+    request(app)
+      .post('/lions')
+      .send({
+        name: 'Mufasa', 
+        age: 111, 
+        pride: 'Evil lions', 
+        gender:'male'
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .end(function(err,res) {
+        expect(res.body).to.be.an('object');
+        done();
+      })
+  });
+
+  it('should DELETE a lion', function(done) {
+    request(app)
+      // first I have to post in order to be able to later delete
+      .post('/lions')
+      .send({
+        name: 'test lion', 
+        age: 100, 
+        pride: 'test lion', 
+        gender: 'female'
+      })
+      .set('Accept', 'application/json')
+      .end(function(err, res) {
+        var lion = res.body;
+        request(app)
+          .delete('/lions/' + lion.id)
+          .end(function(err, res) {
+            expect(res.body).to.eql(lion);
+            done();
+          })
+      })
+  })
+
+  it('should UPDATE a lion', function(done) {
+    request(app)
+    .post('/lions')
+      .send({
+        name: 'test lion', 
+        age: 100, 
+        pride: 'test lion', 
+        gender: 'female'
+      })
+      .set('Accept', 'application/json')
+      .end(function(err, res) {
+        var lion = res.body;
+        request(app)
+          .put('/lions/' + lion.id)
+          .send({
+            name: 'new name'
+          })
+          .end(function(err, res) {
+            expect(res.body.name).to.eql('new name');
+            done();
+          })
+      })
+  })
+
 });
 
 
