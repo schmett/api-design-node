a0c183db3f1ad4e7e6b93b29149d525e82965c3b
Sylvia Yanakieva <sylvia.yanakieva@gmail.com>
2016-11-30 10:49:45 -0800

    Implement refresher 8

--- server/api/post/postModel.js
+++ server/api/post/postModel.js
@@ -2,7 +2,23 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var PostSchema = new Schema({
-
+  title: {
+  	type: String, 
+  	required: true
+  }, 
+  text: {
+  	type: String
+  }, 
+  author: {
+  	type: Schema.Types.ObjectId, 
+  	ref: 'user', 
+  	// user collection we made earlier
+  	required: true
+  }, 
+  categories: [{
+  	type: Schema.Types.ObjectId, 
+  	ref: 'category'
+  }]
 });
 
 module.exports = mongoose.model('post', PostSchema);
--- server/api/user/userModel.js
+++ server/api/user/userModel.js
@@ -2,7 +2,30 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-
+  username: {
+  	type: String, 
+  	required: true, 
+  	unique: true
+  }, 
+  address: {
+  	// address is an object with a state properties
+  	state: {
+  		type: String, 
+  		required: true
+  	}
+  }, 
+  // I have a post model which would allow me to find 
+  // all the data associated with a certain user
+  // ie I go to all the posts and then I can just
+  // find all the posts associated with a certain id
+  // what I can also do however is create a cross-reference
+  // of sorts, like the one below
+  // this is called denormalisation
+  // in other ways, there are more than one ways
+  // to access the information I'm looking for
+  posts: [
+    {ref: 'posts', type: Schema.Types.ObjectId}
+  ]
 });
 
 module.exports = mongoose.model('user', UserSchema);
