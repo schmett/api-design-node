6992c20c341116a2f0b3e25419c18811da9f8a3c
Sylvia Yanakieva <sylvia.yanakieva@gmail.com>
2016-11-30 15:13:25 -0800

    Implement refresher 11

--- server/api/category/categoryRoutes.js
+++ server/api/category/categoryRoutes.js
@@ -1,13 +1,17 @@
 var router = require('express').Router();
 var logger = require('../../util/logger');
-// setup boilerplate route jsut to satisfy a request
+var controller = require('./categoryController');
+// setup boilerplate route just to satisfy a request
 // for building
 router.param('id', function(){});
 
 router.route('/')
-
+  .get(controller.get)
+  .post(controller.post)
 
 router.route('/:id')
-
+  .get(controller.getOne)
+  .put(controller.put)
+  .delete(controller.delete)
 
 module.exports = router;
--- server/api/post/postRoutes.js
+++ server/api/post/postRoutes.js
@@ -1,13 +1,17 @@
 var router = require('express').Router();
 var logger = require('../../util/logger');
-
+var controller = require('./postController');
 // setup boilerplate route jsut to satisfy a request
 // for building
 router.param('id', function(){});
 
 router.route('/')
+  .get(controller.get)
+  .post(controller.post)
 
 router.route('/:id')
-
+  .get(controller.getOne)
+  .update(controller.update)
+  .delete(controller.delete)
 
 module.exports = router;
